087ac8c8ec3046c1206d4936b4a6eb1da1abe5a2
Sumit Kumar <sumitkumar@Sumits-Mac.local>
2019-03-13 19:54:05 -0400

    Camera app code

--- SwiftCameraAndGalleryExample/ViewController.swift
+++ SwiftCameraAndGalleryExample/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
+    
 
+    @IBOutlet weak var imgPhoto: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func btnGallery(_ sender: Any) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let myPickerController = UIImagePickerController()
+            myPickerController.delegate = self as UIImagePickerControllerDelegate & UINavigationControllerDelegate;
+            myPickerController.sourceType = .photoLibrary
+            self.present(myPickerController, animated: true, completion: nil)
+    }
+    }
+    @IBAction func iconCamera(_ sender: Any) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let myPickerController = UIImagePickerController()
+            myPickerController.delegate = self as UIImagePickerControllerDelegate & UINavigationControllerDelegate;
+            myPickerController.sourceType = .camera
+            self.present(myPickerController, animated: true, completion: nil)
+    }
 }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.imgPhoto.image = image
+        }else{
+            print("Something went wrong")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    }
+
+
 
